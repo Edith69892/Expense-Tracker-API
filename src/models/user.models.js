38e006321d5e27a3c2f042f3cb67cefc0cc740f1
Roshan Patil <patilroshan9687@gmail.com>
2025-11-04 14:05:49 +0530

    add hashing password and generate access and refresh token

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,13 +1,49 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
-    userName : String,
-    email : String,
-    password : String,
-    refreshToken : String,
+    userName: String,
+    email: String,
+    password: String,
+    refreshToken: String,
 
-},{timestamps :true})
+}, { timestamps: true })
 
-const User = mongoose.model("User",userSchema)
+const User = mongoose.model("User", userSchema)
 
-module.exports = {User}
+userSchema.pre('save', async function (next) {
+    if (!this.Modified("password")) return next()
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods.isPasswordMatched = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.genrateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: "1D",
+        }
+    );
+
+};
+
+userSchema.methods.genrateRefreshToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: "10d",
+        }
+    );
+};
+
+module.exports = User
