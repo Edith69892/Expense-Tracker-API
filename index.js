0f62de2ac575faca1f23531bc4bee3a3b0483f24
Roshan Patil <patilroshan9687@gmail.com>
2025-11-04 13:37:09 +0530

    connect Db

---
+++ index.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const app = require("./app.js")
+const dotenv = require("dotenv");
+
+
+dotenv.config({ path: "./.env" });
+
+;(async ()=> {
+    try {
+        await mongoose.connect("mongodb+srv://Naruto:123Naruto@cluster0.kktjlvh.mongodb.net/ExpressTracker")
+
+        app.listen(process.env.PORT, () => {
+            console.log("server is running on port :", process.env.PORT);
+            
+        })
+        
+    } catch (error) {
+        console.log("MongoDb connection error :", error)
+    }
+})()
