39f0619645a0f7f07b584a4f098c951c6f3a7d03
Roshan Patil <patilroshan9687@gmail.com>
2025-11-04 15:31:37 +0530

    write logout route

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,10 +1,12 @@
 const  Router = require("express");
-const { register } = require("../controllers/user.controller");
-const { login }  = require("../controllers/user.controller");
+const { register, logOut } = require("../controllers/user.controller.js");
+const { login }  = require("../controllers/user.controller.js");
 
 const router = Router();
 
 router.route("/register").post(register)
 router.route("/login").post(login)
+router.route("/logOut").post(logOut)
+
 
 module.exports = router
