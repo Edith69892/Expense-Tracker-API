ae3e86b5b3c8ce24fa028cd777ebee9ca16b9f37
Roshan Patil <patilroshan9687@gmail.com>
2025-11-04 15:29:57 +0530

    import cookie-Parser

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cookieParser = require('cookie-parser')
 
 
 const app = express();
@@ -6,6 +7,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
     res.send("Hy");
