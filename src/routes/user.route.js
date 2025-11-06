b23ed6efcf94e142539971e287bea501d3dd11ee
Roshan Patil <patilroshan9687@gmail.com>
2025-11-06 12:16:00 +0530

    import verifyJwt for logout

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,12 +1,13 @@
 const  Router = require("express");
 const { register, logOut } = require("../controllers/user.controller.js");
 const { login }  = require("../controllers/user.controller.js");
+const verifyJwt  = require("../middlewares/auth.middleware.js")
 
 const router = Router();
 
 router.route("/register").post(register)
 router.route("/login").post(login)
-router.route("/logOut").post(logOut)
+router.route("/logOut").post(verifyJwt, logOut)
 
 
 module.exports = router
