1b8b0b3ce3abca7e30e487862321508a8d7d8dc6
Roshan Patil <patilroshan9687@gmail.com>
2025-11-06 11:24:19 +0530

    write Expense model

---
+++ src/models/expense.models.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const expenseSchema = new mongoose.Schema({
+    title : String,
+    amount : Number,
+    type : String,
+    category : String,
+    date : Date,
+    owner : {
+        type : mongoose.Types.ObjectId,
+        ref : "User"
+    }
+});
+
+const Expense = mongoose.model("Expense", expenseSchema);
+
+module.exports = {Expense}
