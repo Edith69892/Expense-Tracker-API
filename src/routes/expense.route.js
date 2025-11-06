b7de4e6a2fd47a7c6cf6b150fce4a5c449a1cd10
Roshan Patil <patilroshan9687@gmail.com>
2025-11-06 16:41:27 +0530

    write delete expense route

--- src/routes/expense.route.js
+++ src/routes/expense.route.js
@@ -1,10 +1,11 @@
 const  Router = require("express");
-const { addExpense, updateExpense } = require("../controllers/expense.conntroller.js");
+const { addExpense, updateExpense, deleteExpense } = require("../controllers/expense.conntroller.js");
 const verifyJwt  = require("../middlewares/auth.middleware.js")
 
 const router = Router();
 
 router.route("/").post(verifyJwt,addExpense)
 router.route("/:expenseId").patch(verifyJwt,updateExpense)
+router.route("/:expenseId").delete(verifyJwt,deleteExpense)
 
 module.exports = router
