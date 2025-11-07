dc9b869f1a2dbaf0298994d4a3f1ac87190b4264
Roshan Patil <patilroshan9687@gmail.com>
2025-11-07 13:57:03 +0530

    add getFilteredTransaction route

--- src/routes/expense.route.js
+++ src/routes/expense.route.js
@@ -1,5 +1,5 @@
 const  Router = require("express");
-const { addExpense, updateExpense, deleteExpense, getAllTransactions } = require("../controllers/expense.conntroller.js");
+const { addExpense, updateExpense, deleteExpense, getAllTransactions, getFilteredTransaction } = require("../controllers/expense.conntroller.js");
 const verifyJwt  = require("../middlewares/auth.middleware.js")
 
 const router = Router();
@@ -8,5 +8,6 @@ router.route("/").post(verifyJwt,addExpense)
 router.route("/:expenseId").patch(verifyJwt,updateExpense)
 router.route("/:expenseId").delete(verifyJwt,deleteExpense)
 router.route("/").get(verifyJwt,getAllTransactions)
+router.route("/filter").get(verifyJwt,getFilteredTransaction)
 
 module.exports = router
