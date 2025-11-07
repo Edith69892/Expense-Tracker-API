1ad4fd81972772799349c8e4e0c67eccd6c2eaaf
Roshan Patil <patilroshan9687@gmail.com>
2025-11-07 12:29:59 +0530

    write getAllTransactions route

--- src/routes/expense.route.js
+++ src/routes/expense.route.js
@@ -1,5 +1,5 @@
 const  Router = require("express");
-const { addExpense, updateExpense, deleteExpense } = require("../controllers/expense.conntroller.js");
+const { addExpense, updateExpense, deleteExpense, getAllTransactions } = require("../controllers/expense.conntroller.js");
 const verifyJwt  = require("../middlewares/auth.middleware.js")
 
 const router = Router();
@@ -7,5 +7,6 @@ const router = Router();
 router.route("/").post(verifyJwt,addExpense)
 router.route("/:expenseId").patch(verifyJwt,updateExpense)
 router.route("/:expenseId").delete(verifyJwt,deleteExpense)
+router.route("/").get(verifyJwt,getAllTransactions)
 
 module.exports = router
