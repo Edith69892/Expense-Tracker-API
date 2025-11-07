4a6782dfe5ea407638dedf442309d434ed30b3ca
Roshan Patil <patilroshan9687@gmail.com>
2025-11-07 15:57:43 +0530

    remove dashboard controller and  export getMonthExpense

--- src/controllers/expense.conntroller.js
+++ src/controllers/expense.conntroller.js
@@ -1,6 +1,5 @@
 const { Expense } = require("../models/expense.models.js");
 const User = require("../models/user.models.js");
-const { options } = require("../routes/expense.route.js");
 const ApiError = require("../utils/ApiError.js");
 const ApiResponse = require("../utils/ApiResponse.js");
 const asyncHandler = require("../utils/asyncHandler.js");
@@ -251,50 +250,4 @@ const getMonthExpense = asyncHandler(async (req, res, next) => {
         .json(new ApiResponse(200, "Monthly analytics fetched successfully", expenses));
 })
 
-
-const dashboard = asyncHandler(async (req, res, next) => {
-    const user = await User.findById(req.user?._id);
-
-    if (!user) throw new ApiError(400, "Please loged in or register account.");
-
-    const result = await Expense.aggregate([
-        {
-            $match: { owner: user?._id }
-        },
-        {
-            $group: {
-                _id: null,
-                totalIncome: {
-                    $sum: {
-                        $cond: [
-                            {$eq: ["$type", "income"]},
-                            "$amount",
-                            0
-                        ]
-                    }
-                },
-                totalExpense: {
-                    $sum: {
-                        $cond: [
-                            {$eq: ["$type", "expense"]},
-                            "$amount",
-                            0
-                        ]
-                    }
-                }
-            }
-        }
-    ]);
-
-    const income = result[0]?.totalIncome || 0;
-    const expense = result[0]?.totalExpense || 0;
-    const balance = income - expense;
-
-    return res.status(200).json({
-        income,
-        expense,
-        balance
-    })
-})
-
-module.exports = { addExpense, updateExpense, deleteExpense, getAllTransactions, searchExpense, filteredExpenseByCategory,dashboard }
+module.exports = { addExpense, updateExpense, deleteExpense, getAllTransactions, searchExpense, filteredExpenseByCategory,getMonthExpense }
